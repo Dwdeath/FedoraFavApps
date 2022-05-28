1e9842dbc1c93c883a4dd20867a833ff41d72248
Dwdeath <87183111+Dwdeath@users.noreply.github.com>
2022-05-28 18:52:49 +0300

    Update install.sh
    added variables section

--- install.sh
+++ install.sh
@@ -2,11 +2,14 @@
 ########## Eddie's Personalized Fedora ###############
 ######################################################
 
+# set variables
+apps_list=https://dwdeath.github.io/FedoraFavApps/packages.txt
+
 # First, enable the copr repo
 sudo dnf copr enable @neurofedora/neurofedora-extra
 
 # Enable stuff
-sudo dnf copr enable scorreia/rtl88x2bu
+sudo dnf copr enable $(awk '{print $1}' curl -s apps_list)
 
 # Install them packages
 sudo dnf install rtl88x2bu
